3281724f31296c02d794aaa4169b2f9d2e1f76b7
sk-nagur-baji <109502192+sk-nagur-baji@users.noreply.github.com>
2024-03-15 16:00:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("kilometerstometers = ", metrics.kilometerstometers(x));
+console.log("Meterstocentimeters = ", metrics.meterstocentimers(x));
+console.log("centimeterstomillimeters = ", metrics.centimeterstomillimeters(x));
+console.log("meterstodecimeters = ", metrics.meterstodecimeters(x));
---
+++ metrics.js
@@ -0,0 +1,14 @@
+exports.kilometerstometers = function(x) {
+    return x * 1000;
+};
+exports.meterstocentimers = function(x) {
+    return x * 100;
+};
+
+exports.centimeterstomillimeters = function(x) {
+    return x * 10;
+};
+
+exports.meterstodecimeters = function(x) {
+    return x * 10;
+};
